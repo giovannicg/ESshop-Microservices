f09974ab7eb3df2fe8d779499441f12eb156d1d6
giovannicg <60613860+giovannicg@users.noreply.github.com>
2024-07-17 20:41:52 -0500

    remove carter in building blocks cause the implimentations using libraries

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,7 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddCarter();
+builder.Services.AddCarter(null);
 builder.Services.AddMediatR(config =>
 {
 	config.RegisterServicesFromAssembly(typeof(Program).Assembly);
