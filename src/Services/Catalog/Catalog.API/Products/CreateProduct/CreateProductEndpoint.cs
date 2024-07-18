00c0d2126125e959a2986a5386aadc6cbc309df7
giovannicg <60613860+giovannicg@users.noreply.github.com>
2024-07-17 20:27:09 -0500

    Register MediatR and Carter libraries into Asp.Net Dependency Injection Service and develop POST Endpoint with Carter implements ICarterModule for Minimal Apis

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
@@ -1,6 +1,42 @@
 ﻿namespace Catalog.API.Products.CreateProduct
 {
-	public class CreateProductEndpoint
+	public record CreateProductRequest(string Name, List<string> Category, string Description, string ImageFile, decimal Price);
+
+	public record CreateProductResponse(Guid Id);
+
+	/// <summary>
+	/// Represents the endpoint for creating a new product.
+	/// </summary>
+	public class CreateProductEndpoint : ICarterModule
 	{
+		/// <summary>
+		/// Adds routes for the create product endpoint.
+		/// </summary>
+		/// <param name="app">The endpoint route builder.</param>
+		public void AddRoutes(IEndpointRouteBuilder app)
+		{
+			app.MapPost("/products", async (CreateProductRequest request, ISender sender) =>
+			{
+				/*
+
+				 La línea de código var command = request.Adapt<CreateProductCommand>(); 
+				toma el objeto request de tipo CreateProductRequest y lo adapta o mapea a un objeto CreateProductCommand.
+				Esto permite que los datos del objeto request se asignen a las propiedades correspondientes del objeto command, 
+				de modo que se puedan utilizar para realizar la operación de creación del producto.
+				 */
+				var command = request.Adapt<CreateProductCommand>();
+
+				var result = await sender.Send(command);
+
+				var response = result.Adapt<CreateProductResponse>();
+
+				return Results.Created($"/products/{response.Id}", response);
+			})
+			.WithName("CreateProduct")
+			.Produces<CreateProductResponse>(StatusCodes.Status201Created)
+			.ProducesProblem(StatusCodes.Status400BadRequest)
+			.WithSummary("Create a new product")
+			.WithDescription("Create a new product in the catalog");
+		}
 	}
 }
